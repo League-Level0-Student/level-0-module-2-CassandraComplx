95c747f57e0ab6934ee33fb1fdecc4abdeda86ac
league <karinabriellen@gmail.com>
2024-03-03 11:27:35 -0800

    finished 8 ball started animal farm

--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,20 +6,36 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
+	public static void main(String[] args) {
 
 	// 1. Make a main method that includes all the steps below
-
+		JOptionPane.showInputDialog("ask a question");
 	// 2. Get the user to enter a question for the 8 ball to answer
 	
 
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
 	//     Limit the random numbers to be between 0 and 3
-
-	
+		int number = 0;
+			Random ran = new Random();
+		
+			for (int i=0; i<1; i++) {
+				number = ran.nextInt( 3 - 1 + 1 ) + 1;
+				System.out.println(number);
+			}
 	// 4. If the random number is 0
-
+if (number == 0) {
+	JOptionPane.showMessageDialog(null, "Yes");
+} else if (number == 1) {
+	JOptionPane.showMessageDialog(null, "no");
+} else if (number == 2) {
+	JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
+} else if (number == 3) {
+	JOptionPane.showMessageDialog(null, "maybe");
+}
 	// -- tell the user "Yes"
 
 	// 5. If the random number is 1
@@ -34,4 +50,4 @@ public class Magic8Ball {
 
 	// -- write your own answer
 
-}
+}}
