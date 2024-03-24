9ba893cc1e422e30b327f6af53214df69407dbdb
league <league@192.168.1.200>
2024-03-24 11:26:41 -0700

    worked on animal farm and safe cracker

--- src/_02_methods/_1_animal_farm/AnimalFarm.java
+++ src/_02_methods/_1_animal_farm/AnimalFarm.java
@@ -27,13 +27,14 @@ public class AnimalFarm {
 
 		/* 1. Ask the user which animal they want, then see and hear 
 		 *    the animal they chose using one of the methods below.
-		*/			 
+		*/	
+		String input = JOptionPane.showInputDialog("what animal do you want?");}	 
 			
 		/* 2. Make it so that the user can keep entering new animals. */
 
-	}
+	
 
-	void moo() {
+	void moo() {	
 		playNoise(mooFile);
 		showImage(cowIcon);
 	}
