5ca6ae095e58a3e7e5271991fbaff2058e9feed1
league <league@wts14-3.attlocal.net>
2024-07-14 11:25:28 -0700

    Finished Find the Error worked on NasaCountdown

--- src/_99_extra/NasaCountdown.java
+++ src/_99_extra/NasaCountdown.java
@@ -1,6 +1,8 @@
 
 package _99_extra;
 
+import javax.swing.JOptionPane;
+
 /* NASA wants to shoot a rocket into orbit.
  * Somebody has to do the countdown or the rocket won't launch.
  * 
@@ -13,9 +15,9 @@ package _99_extra;
 public class NasaCountdown {
     public static void main(String[] args) throws InterruptedException {
         // 2. Use a pop-up to ask the user where to start counting from
-        
+    	JOptionPane.showInputDialog(null, "where to start counting from?");
         // 3. Change the countdown to use the new starting point
-        
+    	
         // 1. Print a countdown from 10 to 0 on the console
             
             // 4. Use the Sound.speak method to hear the countdown.
